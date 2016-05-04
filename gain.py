ab22fd8c4a554cff7e35cc851a5cea94265a7791
Simon Vedrine <simon.vedrine@u-psud.fr>
2016-05-04 17:29:26 +0200

    J'ai remplacé le gain exponentiel par un gain linéaire, dans une version très édulcorée le programme marche

--- gain.py
+++ gain.py
@@ -9,64 +9,65 @@ from enum import Enum
 parser = argparse.ArgumentParser(description='added a gain on fields values from a base file, then return the new output file.', usage='cd gprMax; python -m tools.gain basefile initialtime (-exp)')
 parser.add_argument('basefile', help='base name of output file including path')
 parser.add_argument('initialtime', help='time (ns) at which the gain is added')
-parser.add_argument('-exp','--exponential', action="store_true", help='chosen an exponential gain type')
+parser.add_argument('-lin','--linear', action="store_true", help='chosen an linear gain type')
 args = parser.parse_args()
 
 basefile = args.basefile
-tinit = args.initialtime * 10e-9
+tinit = float(args.initialtime)* 1e-9
+print(tinit)
 outputfile = basefile + '_gained.out'
-print(outputfile)
+
 
 # Create output file
-fout = h5py.File(outputfile, 'w')
+#fout = h5py.File(outputfile, 'w')
 
 # Open base file and read some attributes
-f = h5py.File(basefilename, 'r+')
+f = h5py.File(basefile+'.out', 'r+')
 
-itterations = fin.attrs['Iterations'] 
-dt = fin.attrs['dt'] 
-nrx = fin.attrs['nrx']
+iterations = f.attrs['Iterations'] 
+dt = f.attrs['dt'] 
+#rxs = f.attrs['rxs']
 time = np.linspace(0, 1, iterations)
 time *= (iterations * dt)
-
+print(time)
 fgain = []
 	
 # Create gain function chosen by user
-if args.exponential:
+if args.linear:
 
 	for t in time:
-		fgain.append(np.exp(t-tinit))
+		fgain.append(6*t*1e9)
 else:
-	fgain= ones(dim(time))
-	
+	fgain= np.ones(np.size(time))
+
 
 # Find initial time idexation 
 tindex=0
 while time[tindex] <= tinit:
 	tindex += 1 
-
-
+print(tindex)
+rxindex = 0
 # For each rx, add the gain on fields component values 
-for rxindex, rx in enumerate(rxs):
+#for rxindex, rx in enumerate(rxs):
 		
-	if 'Ex' in rx.outputs:
-		f['/rxs/rx' + str(rxindex + 1) + '/Ex'][tindex:] *= fgain[tindex:]
-	if 'Ey' in rx.outputs:
-		f['/rxs/rx' + str(rxindex + 1) + '/Ey'][tindex:] *= fgain[tindex:]
-	if 'Ez' in rx.outputs:
-		f['/rxs/rx' + str(rxindex + 1) + '/Ez'][tindex:] *= fgain[tindex:]
-	if 'Hx' in rx.outputs:
-		f['/rxs/rx' + str(rxindex + 1) + '/Hx'][tindex:] *= fgain[tindex:]
-	if 'Hy' in rx.outputs:
-		f['/rxs/rx' + str(rxindex + 1) + '/Hy'][tindex:] *= fgain[tindex:]
-	if 'Hz' in rx.outputs:
-		f['/rxs/rx' + str(rxindex + 1) + '/Hz'][tindex:] *= fgain[tindex:]
-	if 'Ix' in rx.outputs:
-		f['/rxs/rx' + str(rxindex + 1) + '/Ix'][tindex:] *= fgain[tindex:]
-	if 'Iy' in rx.outputs:
-		f['/rxs/rx' + str(rxindex + 1) + '/Iy'][tindex:] *= fgain[tindex:]
-	if 'Iz' in rx.outputs:
-		f['/rxs/rx' + str(rxindex + 1) + '/Iz'][tindex:] *= fgain[tindex:]
+	#if 'Ex' in rx.outputs:
+f['/rxs/rx' + str(rxindex + 1) + '/Ex'][tindex:] *= fgain[tindex:]
+	#if 'Ey' in rx.outputs:
+f['/rxs/rx' + str(rxindex + 1) + '/Ey'][tindex:] *= fgain[tindex:]
+	#if 'Ez' in rx.outputs:
+f['/rxs/rx' + str(rxindex + 1) + '/Ez'][tindex:] *= fgain[tindex:]
+	#if 'Hx' in rx.outputs:
+f['/rxs/rx' + str(rxindex + 1) + '/Hx'][tindex:] *= fgain[tindex:]
+	#if 'Hy' in rx.outputs:
+f['/rxs/rx' + str(rxindex + 1) + '/Hy'][tindex:] *= fgain[tindex:]
+	#if 'Hz' in rx.outputs:
+f['/rxs/rx' + str(rxindex + 1) + '/Hz'][tindex:] *= fgain[tindex:]
+	#if 'Ix' in rx.outputs:
+f['/rxs/rx' + str(rxindex + 1) + '/Ix'][tindex:] *= fgain[tindex:]
+	#if 'Iy' in rx.outputs:
+f['/rxs/rx' + str(rxindex + 1) + '/Iy'][tindex:] *= fgain[tindex:]
+	#if 'Iz' in rx.outputs:
+f['/rxs/rx' + str(rxindex + 1) + '/Iz'][tindex:] *= fgain[tindex:]
 	
 
 
